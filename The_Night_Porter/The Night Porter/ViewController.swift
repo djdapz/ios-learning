754d1ae8ad6d51fb7f0c2df58c236d1b1510ef0e
devondapuzzo [Stark] <djdapz@aol.com>
2020-08-10 12:52:25 -0500

    add superhero toggling

--- The_Night_Porter/The Night Porter/ViewController.swift
+++ The_Night_Porter/The Night Porter/ViewController.swift
@@ -11,9 +11,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func togglePartyMode(_ sender: Any) {
-        view.backgroundColor = UIColor.purple
+    struct Superhero {
+        var background: UIColor
+        var text: UIColor
+        var name: String
     }
+
+    let superheroColors: [Superhero] = [
+        Superhero(background: UIColor.purple, text: UIColor.green, name: "HULK"),
+        Superhero(background: UIColor.red, text: UIColor.blue, name: "SPIDERMAN"),
+        Superhero(background: UIColor.blue, text: UIColor.red, name: "SUPERMAN"),
+        Superhero(background: UIColor.yellow, text: UIColor.red, name: "IRONMAN"),
+        Superhero(background: UIColor.black, text: UIColor.yellow, name: "BATMAN")
+    ]
+
+    var superheroIndex = 0;
+
+    @IBOutlet weak var superheroLabel: UILabel!
+
+    @IBAction func togglePartyMode(_ sender: Any) {
+        let superhero = superheroColors[superheroIndex]
+
+        view.backgroundColor = superhero.background
+
+        view.subviews.filter {
+                    $0 is UILabel
+                }
+                .map {
+                    $0 as! UILabel
+                }
+                .forEach {
+                    $0.textColor = superhero.text
+                }
+
+        superheroLabel.text = superhero.name
+
+        superheroIndex = (superheroIndex + 1) % superheroColors.count
+
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
