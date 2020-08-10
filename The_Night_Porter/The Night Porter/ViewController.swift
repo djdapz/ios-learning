b5419ce523d306df0b8edb454a22a7cc65a52097
devondapuzzo [Stark] <djdapz@aol.com>
2020-08-10 11:42:42 -0500

    start pluralsight ios tutorial

---
+++ The_Night_Porter/The Night Porter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Devon Dapuzzo on 8/10/20.
+//  Copyright © 2020 Devon Dapuzzo. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBAction func togglePartyMode(_ sender: Any) {
+        view.backgroundColor = UIColor.purple
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
