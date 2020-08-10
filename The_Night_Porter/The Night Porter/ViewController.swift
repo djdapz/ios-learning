bef2ac61c70de929aa1693e49e4b782c3dbebd2c
devondapuzzo [Stark] <djdapz@aol.com>
2020-08-10 13:59:24 -0500

    First table :)

--- The_Night_Porter/The Night Porter/ViewController.swift
+++ The_Night_Porter/The Night Porter/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+struct TaskSection {
+    var title: String
+    var tasks: [String]
+}
 
-class ViewController: UIViewController {
 
-    struct Superhero {
-        var background: UIColor
-        var text: UIColor
-        var name: String
+struct Superhero {
+    var background: UIColor
+    var text: UIColor
+    var name: String
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
     let superheroColors: [Superhero] = [
@@ -24,8 +33,39 @@ class ViewController: UIViewController {
         Superhero(background: UIColor.yellow, text: UIColor.red, name: "IRONMAN"),
         Superhero(background: UIColor.black, text: UIColor.yellow, name: "BATMAN")
     ]
-
     var superheroIndex = 0;
+
+    let tasks = [
+        TaskSection(
+                title: "Daily Tasks",
+                tasks: [
+                    "Check all windows",
+                    "Check all doors",
+                    "Check boiler fuel",
+                    "Check mailbox",
+                    "Empty trash",
+                    "Check pipes",
+                    "Document ~spooky~ happenings",
+                ]
+        ),
+        TaskSection(
+                title: "Weekly Tasks",
+                tasks: [
+                    "Check inside all cabins",
+                    "Flush all lavatories in cabins",
+                    "Walk perimeter of property"
+                ]
+        ),
+        TaskSection(
+                title: "Monthly Tasks",
+                tasks: [
+                    "Test security alarm",
+                    "Test motion detector",
+                    "Test smoke alarm"
+                ]
+        )
+    ]
+
 
     @IBOutlet weak var superheroLabel: UILabel!
 
@@ -47,12 +87,27 @@ class ViewController: UIViewController {
         superheroLabel.text = superhero.name
 
         superheroIndex = (superheroIndex + 1) % superheroColors.count
-
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks[section].tasks.count
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        tasks[section].title
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tasks[indexPath.section].tasks[indexPath.row]
+        return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected \(indexPath.row) in section \(indexPath.section)")
     }
 }
